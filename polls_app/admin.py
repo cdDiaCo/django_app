76d86521b7d641a4a3bb2d21e12b84c3f174cda3
Claudia Cocioaba <claudia.cocioaba@3pillar.corp@tim-lap-124.3pillar.corp>
2019-07-29 17:37:05 +0300

    end of day

--- polls_app/admin.py
+++ polls_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Poll, Question, Answer, Results
+
+admin.site.register(Poll)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Results)
+
+
