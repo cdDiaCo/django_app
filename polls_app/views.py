4911d925e3e994a0d6bf02a412e96ab1cec49143
Claudia Cocioaba <claudia.cocioaba@3pillar.corp@tim-lap-124.3pillar.corp>
2019-08-07 17:49:27 +0300

    end of day

--- polls_app/views.py
+++ polls_app/views.py
@@ -79,7 +79,7 @@ def submit(request, poll_id):
         if int(request.session['current_page']) == int(request.session['total_num_of_pages']):
             total_score = Answer.objects.filter(pk__in=request.session['answers_received']).aggregate(Sum('answer_score'))
             del request.session['current_page']
-            del request.session['total_num_of_pages']
+
             return HttpResponseRedirect(
                 reverse('polls_app:poll_results', args=(poll.id, total_score['answer_score__sum'])))
 
@@ -128,23 +128,74 @@ def get_current_page_questions(current_page, questions_list):
 def results(request, poll_id, total_score):
     poll = get_object_or_404(Poll, pk=poll_id)
     results = Results.objects.filter(poll_id = poll_id).filter()
+    upper_limit = getUpperLimit(results, total_score)
 
+    max_possible_score = False
+    necessary_answers = []
+
+    if upper_limit[1] == isMaxPossibleScore(poll_id, upper_limit[1]):
+        max_possible_score = True
+    else:
+        score_difference = upper_limit[1] - total_score
+        necessary_answers = calculateNecessaryAnswers(poll_id, int(request.session['total_num_of_pages']), score_difference)
+
+
+    del request.session['total_num_of_pages']
+    x = render(request, 'polls_app/poll_results.html', { 'poll': poll, 'total_score':total_score, 'upper_limit': upper_limit[1], 'result_id': upper_limit[0],
+                                                         'necessary_amswers': necessary_answers, 'max_possible_score': max_possible_score })
+
+    del request.session['answers_received']
+    return  x
+
+
+# needs implementation !!!
+def isMaxPossibleScore(poll_id, upper_limit):
+    pass
+
+
+
+def calculateNecessaryAnswers(poll_id, total_pages, score_difference):
+    questions_list = Question.objects.filter(poll_id=poll_id)
+    necessary_answers = []
+
+    for page_num in range(1, total_pages+1): # select only one answer for every page
+        current_page_questions = get_current_page_questions(page_num, questions_list)
+
+        # order answers by score desc
+        answers = Answer.objects.filter(question__in=current_page_questions).select_related().order_by('-answer_score')
+
+        for answer in answers:
+            if answer.answer_score > score_difference : # in this case only one answer is needed to change the result
+
+                if len(necessary_answers) > 0 :
+                    # the list already has other answers with smaller score (from previous pages)
+                    # empty the list. Previously added elements are no longer necessary
+                    while len(necessary_answers) > 0:
+                        necessary_answers.pop()
+
+                necessary_answers.append(answer)
+                return necessary_answers
+            else: # score_difference is too big, more answers are needed to change the result
+                score_sum = sum(int(answer.answer_score) for answer in necessary_answers)
+                if score_sum <= score_difference : # already added answers are not enough, more answers needed
+                    necessary_answers.append(answer)
+                    break # jump to the next page - outer for
+                else: # sum of already added answers are enough to change the result
+                    return necessary_answers
+
+    return []
+
+
+
+def getUpperLimit(results, total_score):
     upper_limit_list = []
 
     for result in results:
-        if total_score > result.result_upper_limit :
+        if total_score > result.result_upper_limit:
             continue
         else:
             upper_limit_list.append((result.id, result.result_upper_limit))
 
-    upper_limit = min(upper_limit_list, key=lambda  x:x[1])
+    upper_limit = min(upper_limit_list, key=lambda x: x[1])
 
-    x = render(request, 'polls_app/poll_results.html', { 'poll': poll, 'total_score':total_score, 'upper_limit': upper_limit[1], 'result_id': upper_limit[0] })
-
-    del request.session['answers_received']
-
-    return  x
-
-
-
-
+    return upper_limit
