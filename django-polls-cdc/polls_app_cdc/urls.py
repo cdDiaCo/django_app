37cc4f4432d4e9662fdc8f996afa87062f5c6299
Claudia Cocioaba <claudia.cocioaba@3pillar.corp@tim-lap-124.3pillar.corp>
2019-08-20 10:53:58 +0300

    packaging files

---
+++ django-polls-cdc/polls_app_cdc/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = 'polls_app_cdc'
+
+urlpatterns = [
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:poll_id>/', views.detail, name='poll_detail'),
+    path('<int:poll_id>/submit/', views.submit, name='poll_submit'),
+    path('<int:poll_id>/<int:total_score>/results/', views.results, name='poll_results'),
+]
+
+
+
+
